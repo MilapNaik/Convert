e9e17fa76cf7829e26d1313a909a307e96a60c13
Milap Naik <milapnaik@gmail.com>
2015-05-04 20:36:24 -0400

    Finished mi to km functionality

--- Convert.java
+++ Convert.java
@@ -8,14 +8,15 @@ import java.util.Scanner;
       {
     
      // declaration of the variables
-     double km, miles;
-     String measure;
-     Boolean KMorMI;
+     double km = 0; 
+     double miles = 0;
+     String measure = "";
      Boolean correct = false;
      
      // create a Scanner
-     Scanner input;
+     Scanner input, distance;
      input = new Scanner(System.in);
+     distance = new Scanner(System.in);
     
      while (!correct) //if correct is true, the user entered either 'km' or 'mi' correctly
      {
@@ -23,12 +24,7 @@ import java.util.Scanner;
 	     System.out.print("Please enter either 'km' or 'mi': ");           
 	     measure = input.toString(); // Read in input from terminal
 	                                  // and stores it in the variable "km"      
-	     if (measure == "km"){
-	    	 KMorMI = false; //false will mean kilometers
-	    	 correct = true;
-	     }
-	     else if (measure == "mi"){
-	    	 KMorMI = true; //true will mean miles
+	     if (measure == "km" || measure == "mi"){
 	    	 correct = true;
 	     }
 	     else
@@ -40,19 +36,35 @@ import java.util.Scanner;
      System.out.print("Please enter a distance in" + measure + ": ");  
      
      if (measure == "km"){
-     km = input.nextDouble(); // Read in input from terminal
-                                  // and stores it in the variable "km"  
-     
-     // Calculate distance in miles
-     miles = 0.62 * km;
-     
-     // print out the result
-     System.out.print(km);
-     System.out.print(" kilometers = ");
-     System.out.print(miles);
-     System.out.println(" miles");
+	     km = distance.nextDouble(); // Read in input from terminal
+	                                  // and stores it in the variable "km"  
+	     
+	     // Calculate distance in miles
+	     miles = 0.62 * km;
+	     // print out the result
+	     System.out.print(km);
+	     System.out.print(" kilometers = ");
+	     System.out.print(miles);
+	     System.out.println(" miles");
+     }
+     else{
+    	miles = distance.nextDouble(); // Read in input from terminal
+        // and stores it in the variable "km"  
+	     
+     	// Calculate distance in miles
+     	km = 1.6 * miles;  
+        // print out the result
+        System.out.print(miles);
+        System.out.print(" miles = ");
+        System.out.print(km);
+        System.out.println(" kilomers");
+      }
+      
+      
+
      
      input.close();
+     distance.close();
       }
     
    }
